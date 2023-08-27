d268d83c6515cb7e4cb3a4e3a0ba222b795e7018
Agustin Torassa <toraagu123@gmail.com>
2023-08-27 10:57:00 -0300

    Still working on collisions with platforms

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const player = document.getElementById('player');
-const game = document.getElementById('game');
+const player = {
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 50,
+    velocityY: 0,
+    isGrounded: false,
+};
 
-let playerX = 0;
-let playerY = 0;
-let playerVelocityY = 0;
-let isGrounded = false;
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Center the canvas and set its size
+canvas.width = 800;
+canvas.height = 600;
+canvas.style.margin = 'auto';
+canvas.style.display = 'block';
+canvas.style.backgroundColor = 'grey';
+
 const jumpStrength = 12;
 const gravity = 0.6;
 
 const keys = {};
-
-const platforms = [];
 
 document.addEventListener('keydown', (event) => {
     event.preventDefault();
@@ -21,96 +31,90 @@ document.addEventListener('keyup', (event) => {
     keys[event.key] = false;
 });
 
+const platforms = [
+    { x: 100, y: 50, width: 100, height: 20 },
+    { x: 300, y: 50, width: 150, height: 20 },
+];
+
 function updatePlayerPosition() {
     if (keys['a']) {
-        playerX -= 5;
+        player.x -= 5;
     }
     if (keys['d']) {
-        playerX += 5;
+        player.x += 5;
     }
-    if (keys['w'] && isGrounded) {
+    if (keys['w'] && player.isGrounded) {
         jump();
     }
 
     // Apply gravity
-    playerVelocityY -= gravity;
-    playerY += playerVelocityY;
-
-    // Keep the player within the game bounds
-    if (playerX < 0) playerX = 0;
-    if (playerX > game.offsetWidth - player.offsetWidth) playerX = game.offsetWidth - player.offsetWidth;
-
-    // Prevent the player from falling below the ground
-    if (playerY < 0) {
-        playerY = 0;
-        playerVelocityY = 0;
-        isGrounded = true;
-    }
-
-    // Check if player touches the ground
-    if (playerY === 0) {
-        isGrounded = true;
-    }
+    player.velocityY -= gravity;
+    player.y += player.velocityY;
 
     // Check for collisions with platforms
     for (const platform of platforms) {
-        if (checkCollision(player, platform)) {
-            const platformRect = platform.getBoundingClientRect();
-            if (playerVelocityY < 0 && playerY > platformRect.top) {
-                playerVelocityY = 0;
-                playerY = platformRect.top;
-                isGrounded = true;
+        if (checkCollisions(player, platform)) {
+            const playerBottom = canvas.height - player.y;
+            const platformTop = canvas.height - platform.y;
+            const platformBottom = canvas.height - (platform.y + platform.height);
+
+            if (player.velocityY > 0 && playerBottom > platformTop && player.y > platformBottom) {
+                player.y = canvas.height - platformTop;
+                player.isGrounded = true;
+            } else if (player.velocityY < 0 && player.y < platformTop && playerBottom > platformTop) {
+                player.y = canvas.height - (platformTop - player.height);
+                player.velocityY = 0;
             }
-        }        
+        }
     }
 
-    player.style.left = playerX + 'px';
-    player.style.bottom = playerY + 'px';
+    // Keep the player within the game bounds
+    if (player.x < 0) player.x = 0;
+    if (player.x > canvas.width - player.width) player.x = canvas.width - player.width;
+
+    // Prevent the player from falling below the ground
+    if (player.y < 0) {
+        player.y = 0;
+        player.velocityY = 0;
+        player.isGrounded = true;
+    } else {
+        player.isGrounded = false; // Player is not on the ground if above the threshold
+    }
 }
+
 
 function jump() {
-    playerVelocityY = jumpStrength;
-    isGrounded = false;
+    player.velocityY = jumpStrength;
+    player.isGrounded = false;
 }
 
-// Create platforms
-createPlatform(100, 50);
-createPlatform(300, 50);
-createPlatform(500, 50);
-createPlatform(700, 50);
-
-function createPlatform(x, y) {
-    const platform = document.createElement('div');
-    platform.classList.add('platform');
-    platform.style.left = x + 'px';
-    platform.style.bottom = y + 'px';
-    game.appendChild(platform);
-    platforms.push(platform);
-}
-
-function checkCollision(player, platform) {
-    const playerRect = player.getBoundingClientRect();
-    const platformRect = platform.getBoundingClientRect();
-    const playerFeet = playerRect.bottom;
-    const platformTop = platformRect.top;
-
-    const verticalDistance = platformTop - playerFeet;
-
+function checkCollisions(rect1, rect2) {
     return (
-        verticalDistance >= 0 && // Player is above the platform
-        verticalDistance <= playerVelocityY && // Prevents passing through the platform
-        playerRect.right >= platformRect.left &&
-        playerRect.left <= platformRect.right
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y + rect1.height > rect2.y &&
+        rect1.y < rect2.y + rect2.height
     );
 }
 
+function drawPlayer() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(player.x, canvas.height - player.y - player.height, player.width, player.height);
+}
+
+function drawPlatforms() {
+    ctx.fillStyle = 'green';
+    for (const platform of platforms) {
+        ctx.fillRect(platform.x, canvas.height - platform.y - platform.height, platform.width, platform.height);
+    }
+}
 
 function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
     updatePlayerPosition();
+    drawPlayer();
+    drawPlatforms();
     requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
-
-// why can't the player collision with platforms check my code?
-
